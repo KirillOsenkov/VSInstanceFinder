b8537e5bd98a276323b9ee2e6e2c195c0d38d4d9
Kirill Osenkov <github@osenkov.com>
2017-04-04 15:06:49 -0700

    Simplify.

--- MSBuildFinder/Program.cs
+++ MSBuildFinder/Program.cs
@@ -1,87 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.VisualStudio.Setup.Configuration;
 
-namespace MSBuildFinder
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        try
         {
-            var validInstances = new List<VisualStudioInstance>();
+            var query = new SetupConfiguration();
+            var e = query.EnumAllInstances();
 
-            try
+            int fetched;
+            var instances = new ISetupInstance[1];
+            do
             {
-                var query = new SetupConfiguration();
-                var e = query.EnumAllInstances();
-
-                int fetched;
-                var instances = new ISetupInstance[1];
-                do
+                // Call e.Next to query for the next instance (single item or nothing returned).
+                e.Next(1, instances, out fetched);
+                if (fetched <= 0)
                 {
-                    // Call e.Next to query for the next instance (single item or nothing returned).
-                    e.Next(1, instances, out fetched);
-                    if (fetched <= 0) break;
+                    break;
+                }
 
-                    var instance = instances[0];
-                    var state = ((ISetupInstance2)instance).GetState();
-                    Version version;
+                var instance = instances[0];
+                var state = ((ISetupInstance2)instance).GetState();
+                Version version;
 
-                    try
-                    {
-                        version = new Version(instance.GetInstallationVersion());
-                    }
-                    catch (FormatException)
-                    {
-                        continue;
-                    }
+                try
+                {
+                    version = new Version(instance.GetInstallationVersion());
+                }
+                catch (FormatException)
+                {
+                    continue;
+                }
 
-                    // If the install was complete and a valid version, consider it.
-                    if (state == InstanceState.Complete)
-                    {
-                        validInstances.Add(new VisualStudioInstance(
-                            instance.GetDisplayName(),
-                            instance.GetInstallationPath(),
-                            version));
-                    }
-                } while (fetched > 0);
-            }
-            catch
-            {
-            }
+                Console.WriteLine($"{instance.GetDisplayName()} {version} {state} {instance.GetInstallationPath()} ");
 
-            foreach (var instance in validInstances)
-            {
-                Console.WriteLine($"{instance.Name} {instance.Path} {instance.Version}");
-            }
+                // If the install was complete and a valid version, consider it.
+                if (state == InstanceState.Complete)
+                {
+                }
+            } while (fetched > 0);
         }
-    }
-
-    /// <summary>
-    /// Wrapper class to represent an installed instance of Visual Studio.
-    /// </summary>
-    internal class VisualStudioInstance
-    {
-        /// <summary>
-        /// Version of the Visual Studio Instance
-        /// </summary>
-        internal Version Version { get; }
-
-        /// <summary>
-        /// Path to the Visual Studio installation
-        /// </summary>
-        internal string Path { get; }
-
-        /// <summary>
-        /// Full name of the Visual Studio instance with SKU name
-        /// </summary>
-        internal string Name { get; }
-
-        internal VisualStudioInstance(string name, string path, Version version)
+        catch
         {
-            Name = name;
-            Path = path;
-            Version = version;
         }
     }
 }
+
